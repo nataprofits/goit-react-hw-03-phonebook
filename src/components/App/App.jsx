0c525c0cd25340ac1893185a29cecf5c1815e165
Tkachenko Natalia <nataprofits@gmail.com>
2023-06-09 13:56:56 +0100

    Update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,12 +8,7 @@ import { Title, Container, Div, DivContacts } from "./App.styled";
 
 export class App extends Component {
     state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
+      contacts: [],
       filter: '',
     };
     addContact = ({ name, number }) => {
@@ -55,6 +50,16 @@ export class App extends Component {
     handleFilter = evt => {
       this.setState({ filter: evt.currentTarget.value });
     };
+    componentDidMount() {
+      const contacts = localStorage.getItem('contacts');
+      const parsContacts = JSON.parse(contacts);
+      if (parsContacts) { this.setState({contacts: parsContacts})}
+    }
+    componentDidUpdate(prevProps, prevState) {
+if (prevState.contacts !== this.state.contacts) {
+  localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+}
+    }
   
     render() {
       const normFilter = this.state.filter.toLocaleLowerCase();
